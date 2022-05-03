9d05c1c620d05aa1704d63fcf14c799481a30c6f
aliyaghobi <aliymix07@gmail.com>
2022-05-03 23:26:16 +0430

    Login_screen last commit

---
+++ lib/screens/Login_screen.dart
@@ -0,0 +1,206 @@
+import 'dart:ui';
+
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:lottie/lottie.dart';
+import 'package:todo/constant.dart';
+import 'package:todo/screens/home_screen.dart';
+import 'package:todo/screens/sign_up_screen.dart';
+import 'package:todo/services/auth_services.dart';
+import 'package:todo/widgets/custom_button.dart';
+import 'package:todo/widgets/custom_text.dart';
+
+class LoginScreen extends StatefulWidget {
+  static String id = '/LoginScreen';
+
+  const LoginScreen({Key? key}) : super(key: key);
+
+  @override
+  State<LoginScreen> createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen> {
+  TextEditingController emailController = TextEditingController();
+  TextEditingController passwordController = TextEditingController();
+
+  CollectionReference users = FirebaseFirestore.instance.collection('users');
+
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+  final authServices=Get.find<AuthServices>();
+  var currentStatus='Not Login';
+
+  // bool _isValid = false;
+  //
+  // void _saveForm() {
+  //   setState(() {
+  //     _isValid = _formKey.currentState!.validate();
+  //   });
+  // }
+@override
+  void initState() {
+  super.initState();
+  _auth.authStateChanges()
+      .listen((User? user) {
+    if (user == null) {
+      Get.toNamed(SignUpScreen.id);
+    } else {
+      print('User is signed in!');
+      Get.offNamed(HomeScreen.id);
+    }
+  });
+  }
+  @override
+  Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
+    return Scaffold(
+      body: Form(
+        key: _formKey,
+        child: Container(
+          decoration: BoxDecoration(
+            image: DecorationImage(
+              image: ExactAssetImage('assets/images/background2.jpg'),
+              fit: BoxFit.cover,
+            ),
+          ),
+          child: BackdropFilter(
+            filter: ImageFilter.blur(sigmaX: 5.0, sigmaY: 5.0),
+            child: SafeArea(
+              child: Container(
+                margin: EdgeInsets.symmetric(horizontal: 10),
+                child: Column(
+                  children: [
+                    Expanded(
+                      child: SingleChildScrollView(
+                        child: Column(
+                          children: [
+                            SizedBox(
+                              height: 5,
+                            ),
+                            Lottie.asset('assets/images/signup.json',
+                                height: 250,
+                                width: 250,
+                                frameRate: FrameRate(30)),
+                            SizedBox(
+                              height: 5,
+                            ),
+                            Row(
+                              children: [
+                                CustomText(text: 'Login'),
+                              ],
+                            ),
+                            Container(
+                              padding: EdgeInsets.symmetric(
+                                  vertical: 20, horizontal: 5),
+                              margin: EdgeInsets.symmetric(vertical: 5),
+                              decoration: BoxDecoration(
+                                  color: Colors.white24,
+                                  borderRadius: BorderRadius.circular(20)),
+                              child: Column(
+                                mainAxisAlignment:
+                                    MainAxisAlignment.spaceEvenly,
+                                children: [
+                                  SizedBox(
+                                    height: 10,
+                                  ),
+                                  TextFormField(
+                                    keyboardType: TextInputType.emailAddress,
+                                    decoration: InputDecoration(
+                                      hintText: 'Email',
+                                      icon: Icon(
+                                        Icons.email_outlined,
+                                        size: 30,
+                                      ),
+                                    ),
+                                    controller: emailController,
+                                    textInputAction: TextInputAction.next,
+                                    validator: validateEmail,
+                                  ),
+                                  SizedBox(
+                                    height: 1,
+                                  ),
+                                  TextFormField(
+                                    validator: validatePassword,
+                                    decoration: InputDecoration(
+                                      hintText: 'Password',
+                                      icon: Icon(
+                                        Icons.lock_outline,
+                                        size: 30,
+                                      ),
+                                    ),
+                                    controller: passwordController,
+                                    textInputAction: TextInputAction.done,
+                                  ),
+                                  SizedBox(
+                                    height: 20,
+                                  ),
+                                  CustomButton(
+                                    text: 'Login',
+                                    size: size,
+                                    onTapped: () {
+                                      //complete later
+                                      if (_formKey.currentState!.validate()) {
+                                        _formKey.currentState!.save();
+                                        print('Form Submit');
+                                      }
+                                      authServices.login(emailController.text, passwordController.text, context);
+                                    },
+                                  ),
+                                  TextButton(
+                                      onPressed: () {
+                                        Get.toNamed(SignUpScreen.id);
+                                      },
+                                      child: Text(
+                                        'Dont have an account?',
+                                        style: kAccountTextStyle,
+                                      ))
+                                ],
+                              ),
+                            )
+                          ],
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
+  // void checkUser(){
+  //   User? user=_auth.currentUser;
+  //   if(user == null){
+  //     print('not Login');
+  //     setState(() {
+  //       currentStatus='Not Login';
+  //     });
+  //   }
+  //   else{
+  //     print('user:${user.uid}');
+  //     setState(() {
+  //       currentStatus=user.email!;
+  //     });
+  //   }
+  // }
+
+  // void login({var mail, var pass}) async {
+  //   final User? user = (await _auth.signInWithEmailAndPassword(
+  //       email: mail, password: pass))
+  //       .user;
+  //
+  //   if (_auth.currentUser ==null) {
+  //     print("sign up failed!!");
+  //     Get.toNamed(SignUpScreen.id);
+  //   } else {
+  //     print("user Login..");
+  //    Get.offNamed(HomeScreen.id);
+  //   }
+  // }
+}
