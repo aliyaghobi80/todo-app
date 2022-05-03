e1a618a383f102c2e1e2fd13c5fe785e5ad4e28a
aliyaghobi <aliymix07@gmail.com>
2022-05-03 23:24:01 +0430

    import library

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,10 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:todo/screens/Login_screen.dart';
+import 'package:todo/screens/home_screen.dart';
+import 'package:todo/screens/sign_up_screen.dart';
+import 'package:todo/services/auth_services.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
