bf0996e870795236e3990b5776591a874b49f516
aliyaghobi <aliymix07@gmail.com>
2022-05-06 22:09:47 +0430

    changed design home screen and added property bottomNavigationBar

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -24,16 +24,62 @@ class _HomeScreenState extends State<HomeScreen> {
         title: Text('ToDo'),
         centerTitle: true,
       ),
-      body: SafeArea(
-        child: Column(
-          children: [
-        TableCalendar(
-        firstDay: DateTime.utc(2010, 10, 16),
-        lastDay: DateTime.utc(2030, 3, 14),
-        focusedDay: DateTime.now(),
+      body: _pages[_selectedIndex],
+      bottomNavigationBar: BottomNavigationBar(
+        backgroundColor: Colors.grey[900],
+        currentIndex: _selectedIndex,
+        onTap: _navigateBottomBar,
+        type: BottomNavigationBarType.fixed,
+        items: const [
+          BottomNavigationBarItem(
+              icon: Icon(Icons.calendar_today), label: 'Calendar'),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.playlist_add_check), label: 'AddEvent'),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.account_circle), label: 'Account'),
+          BottomNavigationBarItem(icon: Icon(Icons.settings), label: 'Setting'),
+        ],
       ),
-          ],
-        ),
+    );
+  }
+
+  final List<Widget> _pages = [
+    CalendarScreen(),
+    AddEventsScreen(),
+    //if user login return PersonInfo Screen else Show a message , It no displays errors
+    StreamBuilder<User?>(
+      stream: FirebaseAuth.instance.authStateChanges(),
+      builder: (context, snapshot) {
+        if (snapshot.hasData) {
+          return PersonInfo();
+        } else {
+          return Center(
+            child: Text('You are not logged in. Please log in and try again'),
+          );
+        }
+      },
+    ),
+    Center(
+      child: Text('SettingPage'),
+    ),
+  ];
+
+  void _navigateBottomBar(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+  Widget buildDateTimePicker(String data) {
+    return ListTile(
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(10.0),
+        side: BorderSide(color: Colors.green, width: 1.5),
+      ),
+      title: Text(data),
+      trailing: const Icon(
+        Icons.calendar_today,
+        color: Colors.purple,
       ),
     );
   }
